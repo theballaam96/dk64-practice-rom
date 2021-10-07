1573aef4e82e4698482914e891f36815342acf41
theballaam96 <theballaam96@gmail.com>
2021-10-07 11:12:12 -0500

    Auto BPS Patcher & Symbols Map Generator

--- src/build.bat
+++ src/build.bat
@@ -10,12 +10,15 @@ move *.o obj/
 armips asm/jump_list.asm
 cd ".\..\Build\"
 python build.py
-if "%arg1%" == "--generate-watch" (
-	python generate_watch_file.py
-	echo Generated Watch File
-)
+python generate_watch_file.py
+echo Generated Watch File
 cd ".\..\src\"
-armips asm/main.asm
+armips asm/main.asm -sym symbols_map.txt
 python cleanup.py
 n64crc "rom/dk64-practice-rom-dev.z64"
+if "%arg1%" == "--bps" (
+	cd ".\..\Build\flips"
+	echo Building BPS Patch
+	flips --create ".\..\..\src\rom\dk64.z64" ".\..\..\src\rom\dk64-practice-rom-dev.z64" ".\..\..\src\rom\dk64-practice-rom-dev.bps"
+)
 echo Completed: %date% %time%
